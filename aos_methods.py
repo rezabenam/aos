63e3646e1912a74a731be6bc063f8f88d8c0c1bf
Reza Benam <reza@Rezas-MacBook-Pro.local>
2022-03-15 17:32:28 -0700

    the options was addede for awsaos running

--- aos_methods.py
+++ aos_methods.py
@@ -10,11 +10,26 @@ from selenium.webdriver.support.ui import Select # <-- add this import for drop 
 from selenium.webdriver.common.keys import Keys
 
 
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(options=options)
+
+
 print('---------------*%*----------------')
 print('---------------*%*----------------')
 
-s = Service(executable_path='/Users/reza/PycharmProjects/pythonProject/chromedriver')
-driver = webdriver.Chrome(service=s)
+# s = Service(executable_path='/Users/reza/PycharmProjects/pythonProject/chromedriver')
+# driver = webdriver.Chrome(service=s)
 
 
 def setUp():
